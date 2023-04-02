1eeb27d9dcb21980a4a7d31f855f13b198d7f457
Zeneke <hectorvillarm@gmail.com>
2023-04-02 19:47:47 +0200

    - Combo message.

--- src/components/combos/combo.message.js
+++ src/components/combos/combo.message.js
@@ -1,26 +1,43 @@
 import { calculateCombo, isMegaComboById } from "@/domain/combo/combo"
+import { range } from "@/lib/math/random"
 import { applyMegaComboEffect } from './combo.effect'
 
+const TAG_NAME = 'span'
 const COMBO_CLASSNAME = 'combo'
-const DELAY_TO_BE_REMOVED_IN_MS = 3000
+const DELAY_TO_BE_REMOVED_IN_MS = 2000
 
 const wrapperCombos = document.getElementById('combos')
 
-function appendComboElement ({ text, classname }) {
-  const comboElement = document.createElement('span')
-  comboElement.classList.add(COMBO_CLASSNAME, `--${COMBO_CLASSNAME}-${classname}`)
-  comboElement.innerText = text
+const $el = (element) => ({
+  setRandomPosition() {
+    element.style.left = `${range(45, 60)}%`
+    element.style.top = `${range(40, 35)}%`
+    return this
+  },
+  applyStyle(classname) {
+    element.classList.add(COMBO_CLASSNAME, `--${COMBO_CLASSNAME}-${classname}`)
+    return this
+  },
+  setMessage(text) {
+    element.innerText = text
+    return this
+  }
+})
+
+function appendComboElement({ text, classname }) {
+  const comboElement = document.createElement(TAG_NAME)
+  $el(comboElement).applyStyle(classname).setMessage(text).setRandomPosition()
   wrapperCombos.appendChild(comboElement)
   return comboElement
 }
 
-function removeComboElement (comboElement) {
+function removeComboElement(comboElement) {
   setTimeout(() => {
     wrapperCombos.removeChild(comboElement)
   }, DELAY_TO_BE_REMOVED_IN_MS)
 }
 
-function showComboMessage (value) {
+function showComboMessage(value) {
   const { id: comboId, classname, getText } = calculateCombo(value)
   const text = getText(value)
   const comboElement = appendComboElement({ text, classname })
