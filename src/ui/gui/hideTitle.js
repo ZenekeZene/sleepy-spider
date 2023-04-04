6029972ec1c15733d950cf5da1abb43807c7c143
Zeneke <hectorvillarm@gmail.com>
2023-04-04 21:55:57 +0200

    - Hide title and relationated automatically.

--- src/ui/gui/hideTitle.js
+++ src/ui/gui/hideTitle.js
@@ -4,6 +4,8 @@ const HIDE_TITLE_DELAY_IN_MS = 3000
 
 const hideTitle = () => {
   const title = document.getElementById('title')
+  const hidesAuto = document.getElementsByClassName('hide-auto')
+
   let isFirstClick = false
 
   document.addEventListener('click', async () => {
@@ -12,9 +14,25 @@ const hideTitle = () => {
 
     await delay(HIDE_TITLE_DELAY_IN_MS)
     title.classList.add('title-hidden')
+
+    for (var i = 0; i < hidesAuto.length; i++) {
+      hidesAuto[i].classList.add('invisible')
+    }
    })
 }
 
+const showTitle = () => {
+  const title = document.getElementById('title')
+  const hidesAuto = document.getElementsByClassName('hide-auto')
+
+  title.classList.remove('title-hidden')
+
+  for (var i = 0; i < hidesAuto.length; i++) {
+    hidesAuto[i].classList.remove('invisible')
+  }
+}
+
 export {
+  showTitle,
   hideTitle,
 }
