e4336ec3604409ab03057589a7f52dce43f4036f
Zeneke <hectorvillarm@gmail.com>
2023-04-04 19:19:01 +0200

    - Megacombo applies the correct node element.

--- src/components/combos/combo.effect.js
+++ src/components/combos/combo.effect.js
@@ -1,11 +1,13 @@
 const DELAY_MEGACOMBO_EFFECT_IN_MS = 1000
 const MEGACOMBO_EFFECT_CLASSNAME = 'big-surprise'
 
+const element = document.getElementById('spider-wrapper')
+
 function applyMegaComboEffect () {
-  document.body.classList.add(MEGACOMBO_EFFECT_CLASSNAME)
+  element.classList.add(MEGACOMBO_EFFECT_CLASSNAME)
 
   setTimeout(() => {
-    document.body.classList.remove(MEGACOMBO_EFFECT_CLASSNAME)
+    element.classList.remove(MEGACOMBO_EFFECT_CLASSNAME)
   }, DELAY_MEGACOMBO_EFFECT_IN_MS)
 }
 
