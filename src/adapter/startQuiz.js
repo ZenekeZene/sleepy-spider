6ae036bcaa4e31f2696674a570641e18a1f2de2a
¨hvillar¨ <hvillar@idealista.com>
2023-07-19 18:23:13 +0200

    startQuiz simplify

--- src/adapter/startQuiz.js
+++ src/adapter/startQuiz.js
@@ -6,29 +6,33 @@ import { getQuestions } from '@/infra/questions/questions.local.repository'
 import { drawSpider } from '@/ui/components/sleepy/spider/drawSpider'
 import { listenAnsweredCorrect, updateAwakeningsCounter } from '@/ui/components/awakeningCounter/drawAwakeningCount'
 
-async function startQuiz (spiderImage, onShowQuestion) {
+const onChange = (value, spider) => {
+  updateAwakeningsCounter(value)
+  spider.body.incrementHateLevel(value)
+}
+
+const onShowQuestion = (questions, spider) => {
+  spider.body.resetHateLevel()
+  onShowQuestion(questions)
+}
+
+async function startQuiz (image, onShowQuestion) {
   let spider = null
 
   try {
     const questions = await getQuestions({ size: QUESTIONS_MAX_PER_QUIZ })
 
-    const addAwakening = await startAwakeningsSystem({
-      onChange: (value) => {
-        updateAwakeningsCounter(value)
-        spider.body.incrementHateLevel(value)
-      },
-      onShowQuestion: () => {
-        spider.body.resetHateLevel()
-        onShowQuestion(questions)
-      },
-    })
+    const handlers = {
+      onChange: (value) => onChange(value, spider),
+      onShowQuestion: () => onShowQuestion(questions, spider),
+    }
 
+    const addAwakening = startAwakeningsSystem(handlers)
     listenAnsweredCorrect()
-    spider = await drawSpider({ spiderImage, params, onInterruptedSleep: addAwakening })
+    spider = await drawSpider({ image, params, onInterruptedSleep: addAwakening })
   } catch (error) {
     console.error(error)
     alert('Sorry, Sleepy is sleeping deeply. Please try again later.')
-    hasError = true
     findById('spider-wrapper').innerHTML = ''
   }
 }
