0e7fcb2a7179154cd3a849d940bcd1a6b7b548f3
¨hvillar¨ <hvillar@idealista.com>
2023-08-03 16:48:38 +0200

    Listening first click only in collider element

--- src/adapter/events/listenFirstClick.js
+++ src/adapter/events/listenFirstClick.js
@@ -1,10 +1,12 @@
+import { findById } from 'sleepy-spider-lib'
 import { EVENTS } from '@/adapter'
-import { dispatchEvent, listenEvent } from "sleepy-spider-lib"
+import { dispatchEvent } from "sleepy-spider-lib"
 
 let isFirstClick = false
 
 function listenFirstClick() {
-  listenEvent('click', async () => {
+  const collider = findById('collider')
+  collider.addEventListener('click', async (event) => {
     if (isFirstClick) return
     isFirstClick = true
     dispatchEvent(EVENTS.FIRST_CLICK)
