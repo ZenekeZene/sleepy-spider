13f40cff56c3ec4463ad83a93499cf53dad5618e
¨hvillar¨ <hvillar@idealista.com>
2023-08-03 16:44:19 +0200

    Responsive adjusts.

--- src/ui/gui/hideTitle.js
+++ src/ui/gui/hideTitle.js
@@ -4,9 +4,9 @@ import {
   findById,
   listenEvent,
   delay,
-  isMobile
+  isMobile,
 } from 'sleepy-spider-lib'
-import { INVISIBLE_CLASS } from '@/ui/constants'
+import { HIDDEN_CLASS, INVISIBLE_CLASS } from '@/ui/constants'
 import { EVENTS } from '@/adapter'
 
 const TITLE_ID = 'title'
@@ -19,10 +19,16 @@ const hideTitleOnFirstClick = () => {
   const title = findById(TITLE_ID)
   const userTitle = findById(USER_TITLE_ID)
   const hidesAuto = findAllByClassName(HIDE_CLASSNAME)
+  const userCounter = findById('user-counter')
+  const infoIcon = findById('info-icon')
+  const clock = findById('clock')
 
   listenEvent(EVENTS.FIRST_CLICK, async () => {
-    if (!isMobile()) return
+    $class.remove(userCounter, HIDDEN_CLASS)
+    $class.add(infoIcon, HIDDEN_CLASS)
+    $class.remove(clock, HIDDEN_CLASS)
 
+    if (!isMobile()) return
     await delay(HIDE_TITLE_DELAY_IN_MS)
     $class.add(title, HIDE_TITLE_CLASSNAME)
     $class.add(userTitle, HIDE_TITLE_CLASSNAME)
