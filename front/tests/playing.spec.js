20c187f19753f3272b597e1e27982c9604cf5174
Zeneke <hectorvillarm@gmail.com>
2023-08-14 21:40:00 +0200

    Playing e2e spec

---
+++ front/tests/playing.spec.js
@@ -0,0 +1,59 @@
+import { test, expect } from '@playwright/test'
+
+test.use({
+  viewport: { width: 1600, height: 1000 },
+})
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/')
+})
+
+const getClock = async (page) => page.locator('#clock')
+
+const doClickOnSpider = async (page, delay = 250) => {
+  const spider = page.locator('#spider')
+  await page.waitForTimeout(delay);
+  await spider.click()
+}
+
+test.describe('Playing [desktop]:', () => {
+  test(`The user can click on spider and
+    the counter is incremented`, async ({ page }) => {
+    await doClickOnSpider(page)
+
+    const counter = page.getByText('1', { exact: true })
+    await expect(counter).toBeVisible()
+
+    await doClickOnSpider(page)
+
+    const counter2 = page.getByText('2')
+    await expect(counter2).toBeVisible()
+  })
+
+  test(`The user can click on the spider,
+    and the info modal is not visible and
+    the clock is visible`, async ({ page }) => {
+    await doClickOnSpider(page)
+
+    const infoIcon = page.locator('#info-icon')
+    expect(infoIcon).not.toBeVisible()
+
+    const clock = await getClock(page)
+    await expect(clock).toBeVisible()
+  })
+
+  test(`The user can click on the spider
+    five times in one second, then the counter
+    is incremented until 5 and the clock decrement
+    one second`, async ({ page }) => {
+    for (let i = 0; i < 6; i++) {
+      await doClickOnSpider(page)
+    }
+
+    const counter = page.getByText('5', { exact: true })
+    await expect(counter).toBeVisible()
+
+    const clock = await getClock(page)
+    await expect(clock).toHaveText('9')
+  })
+})
