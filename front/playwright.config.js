8b3e98f2ec93f3261672d81eee253d72b73dbe98
Zeneke <hectorvillarm@gmail.com>
2023-08-17 22:03:09 +0200

    Touch-action

--- front/playwright.config.js
+++ front/playwright.config.js
@@ -22,6 +22,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? 'html': 'dot',
+  outputDir: './tests/results',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
