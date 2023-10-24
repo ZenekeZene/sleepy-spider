b68e0d67aa99de6eeef82a6d8712ac8e0b465ae6
¨hvillar¨ <hvillar@idealista.com>
2023-10-24 17:54:21 +0200

    fake players with images

--- front/src/infra/leaderboard/fake.players.js
+++ front/src/infra/leaderboard/fake.players.js
@@ -4,24 +4,28 @@ const fakePlayers = [
 		displayName: 'Foo, the king of bar',
 		value: 345,
 		position: 1,
+		photoURL: 'https://i.pravatar.cc/50',
 	},
 	{
 		userUid: '2',
 		displayName: 'FooBar_XXX',
 		value: 250,
 		position: 2,
+		photoURL: 'https://i.pravatar.cc/50',
 	},
 	{
 		userUid: '3',
 		displayName: 'I am not fake',
 		value: 142,
 		position: 3,
+		photoURL: 'https://i.pravatar.cc/50',
 	},
 	{
 		userUid: '4',
 		displayName: 'The mega bot',
 		value: 111,
 		position: 4,
+		photoURL: 'https://i.pravatar.cc/50',
 	},
 ]
 
